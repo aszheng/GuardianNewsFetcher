34a628077a75e50d3774c3b7e6b72f5baff1ebe8
Alan Zheng <aszheng12@gmail.com>
2020-05-24 01:58:49 -0700

    Added search bar

--- app/src/main/java/com/example/guardiannewsfetcher/MainActivity.java
+++ app/src/main/java/com/example/guardiannewsfetcher/MainActivity.java
@@ -3,17 +3,42 @@ package com.example.guardiannewsfetcher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText mSearchEditText;
+    Button mSubmit;
+    String searchString;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSearchEditText = findViewById(R.id.search_et);
+        mSubmit = findViewById(R.id.search_submit);
+
+        mSubmit.setOnClickListener(
+            new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    searchString = mSearchEditText.getText().toString();
+                    Log.d("HELLO WORLD", searchString);
+                    Toast.makeText(MainActivity.this, searchString, Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+        );
 
         ArrayList<Article> articles = new ArrayList<Article>();
 
@@ -31,5 +56,7 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.listview_main);
         listView.setAdapter(articleAdapter);
     }
+
+
 
 }
